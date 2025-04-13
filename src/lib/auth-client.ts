bd10be9d396758c54615816853e871ce82946cd4
Reinier <39220977+RvDstudio@users.noreply.github.com>
2025-04-13 13:00:54 +0200

    Added process.env to the auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,6 +4,6 @@ import { createAuthClient } from "better-auth/react"
 import { magicLinkClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
     plugins: [magicLinkClient()]
 })
